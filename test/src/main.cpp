16d6218ccacee849baf8b93860ecf64cbff488c9
oceancx <oceancx@gmail.com>
2019-04-15 19:50:19 +0800

    for integrate in SimpleEngine

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -14,16 +14,15 @@ extern "C" {
 
 #include <luadbg.h>
 
-#pragma comment(lib, "D:\\Github\\luadebugger\\luadbg\\Debug\\luadbg.lib")
 
 int main(int argc ,char** argv)
 {
 	lua_State * L = luaL_newstate();
 	luaL_openlibs(L); 
 	luaopen_luadbg(L);
-	luaL_dofile(L, "D:/Github/luadebugger/test/main.lua");
+	luaL_dofile(L, "main.lua");
 	
-	printf("%s\n", lua_tostring(L, -1));
+	printf("%s\\n", lua_tostring(L, -1));
 
 	return 0;
 }
